cb6b4bcb10ae9d1a022a7a953d23a3ecbe7befa6
Uma Adhikari <adhikaryuma26@gmail.com>
2022-03-12 22:10:30 -0500

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+# function to check string is
+# palindrome or not
+def isPalindrome(str):
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str) / 2)):
+        if str[i] != str[len(str) - i - 1]:
+            return False
+    return True
+
+
+# main function
+s = input(f'Enter the string to check for palindrome:')
+ans = isPalindrome(s)
+
+if (ans):
+    print(f'{s} is a palindrome')
+else:
+    print(f'{s} is not a palindrome')
